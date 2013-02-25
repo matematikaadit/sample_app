1e46482f1e5dc7d50e00614599d2925018277c85
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-02-26 04:18:24 +0700

    reduce repetition in static_pages_spec.rb, exercise 3.5 #2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +14,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "#{base_title} | Home")
     end
   end
 
@@ -25,7 +28,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -39,7 +42,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :title => "Ruby on Rails Tutorial Sample App | About Us")
+                                :title => "#{base_title} | About")
     end
   end
 
@@ -53,7 +56,7 @@ describe "Static pages" do
     it "should have the title 'Contact'" do
       visit "/static_pages/contact"
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Contact")
+                                :text => "#{base_title} | Contact")
     end
   end
 end
