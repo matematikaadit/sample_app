aabf763ac05bbf0c3ecdd6cf4fff2e433601d224
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-03-09 09:47:03 +0700

    exercise chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user, only: :destroy
+  before_filter :access_forbidden, only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
@@ -48,6 +49,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def access_forbidden
+      redirect_to(root_path, notice: "access forbidden") if signed_in?
+    end
   
     def signed_in_user
       unless signed_in?
